49056b1843cd34c2d45bd0f90e90ac73d668fa66
Łukasz <lukaszdudziak777@gmail.com>
2019-01-28 15:28:58 +0100

    no.7

---
+++ src/r07/PoinyTwo.java
@@ -0,0 +1,21 @@
+package r07;
+
+public class PoinyTwo {
+
+    public static double squareRoot( double x ){
+
+        double i;
+        double squareRoot = x/2.0;
+        do {
+            i = squareRoot;
+            squareRoot = (i + (x/i))/2.0;
+            System.out.println(i);
+            System.out.println(squareRoot);
+        } while ((i - squareRoot) != 0.0001);
+        return squareRoot;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(squareRoot(15));
+    }
+}
